6da4829ffc85d0504c7c6cbb1cb3fe9869eb031f
togra1997 <ryoota1997@gmail.com>
2021-02-14 15:07:11 +0900

    エラー画面をinputで表示できるようにしました

--- src/main/java/com/tiscon/controller/EstimateController.java
+++ src/main/java/com/tiscon/controller/EstimateController.java
@@ -76,7 +76,13 @@ public class EstimateController {
      * @return 遷移先
      */
     @PostMapping(value = "submit", params = "confirm")
-    String confirm(UserOrderForm userOrderForm, Model model) {
+    String confirm(@Validated UserOrderForm userOrderForm, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+
+            model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
+            model.addAttribute("userOrderForm", userOrderForm);
+            return "input";
+        }
 
         model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
         model.addAttribute("userOrderForm", userOrderForm);
